d0f54dd3c7af3911bfb1e8031c962144949836d8
Halil Nebioğlu <halil.nebioglu@ozu.edu.tr>
2023-12-14 16:09:10 +0300

    First

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by in021397 on 12/14/2023.
+//
+#include <iostream>
+#include "Array/Element.h"
+
+using namespace array;
+using namespace std;
+
+int main(){
+
+    Element e = Element();
+    cout<<"af";
+    return 0;
+}
